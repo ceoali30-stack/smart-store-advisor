3cc5677cea2b2ec84225e8812e60e8a15e69f3bc
ceoali30-stack <ceoali30@gmail.com>
2026-05-29 19:37:52 +0300

    Update page.js

--- app/embedded/dashboard/page.js
+++ app/embedded/dashboard/page.js
@@ -1,18 +1,11 @@
-import { redirect } from "next/navigation";
-
-export default async function EmbeddedDashboardPage({ searchParams }) {
+export default async function EmbeddedDashboardPage({
+  searchParams,
+}) {
   const params = await searchParams;
 
-  const merchantId =
-    params?.merchant_id ||
-    params?.merchant ||
-    params?.store_id ||
-    params?.store ||
-    null;
-
-  if (!merchantId) {
-    redirect("/");
-  }
-
-  redirect(`/dashboard?merchant_id=${merchantId}`);
+  return (
+    <pre>
+      {JSON.stringify(params, null, 2)}
+    </pre>
+  );
 }
